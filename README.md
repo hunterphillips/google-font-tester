cdffa5837971c8613018ad56009c4a53221929dd
hunterphillips <hkphillips42@gmail.com>
2023-05-04 08:21:26 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Google Font Tester
+
+[Chrome web store](https://chrome.google.com/webstore/category/extensions?hl=en)
+
+Chrome browser extension used to preview the current page with a selection of Google fonts
---
+++ popup.js
@@ -0,0 +1,34 @@
+const fontSelector = document.getElementById('font-select');
+
+fontSelector.addEventListener('change', async (event) => {
+  const selectedFont = event.target.value;
+  const googleFontUrl = selectedFont
+    ? `https://fonts.googleapis.com/css2?family=${selectedFont}:wght@400;700&display=swap`
+    : '';
+
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: setPageFont,
+    args: [googleFontUrl, selectedFont],
+  });
+});
+
+const setPageFont = (url, fontFamily) => {
+  const existingLink = document.querySelector('#google-fonts-extension-link');
+
+  if (existingLink) existingLink.href = url;
+  else {
+    const link = document.createElement('link');
+    link.id = 'google-fonts-extension-link';
+    link.rel = 'stylesheet';
+    link.href = url;
+    document.head.appendChild(link);
+  }
+
+  document.body.style.fontFamily = fontFamily;
+};
